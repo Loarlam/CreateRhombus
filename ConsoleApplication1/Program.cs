60f2ed4089de452b72eeccc3456bff8594661706
Loarlam <l9m_pam@live.ru>
2020-01-24 20:55:11 +0300

    Добавил комментарий

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*В консольке вводим числовое значение линий до центральной линии ромба*/
+using System;
 
 namespace ConsoleApplication1
 {
